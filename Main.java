40867ca4529330a796c9a0fc725c5257844c9d37
killayera <130810825+killayera@users.noreply.github.com>
2023-07-30 00:15:02 +0600

    Add vertices to the graph

--- Main.java
+++ Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+
+        MyDirectedGraph<Integer> graph = new MyDirectedGraph<>();
+
+        // Add vertices to the graph
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+    }
 }
