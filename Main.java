da495e8587cbcde220c05e5fc5e5010d3846db4b
killayera <130810825+killayera@users.noreply.github.com>
2023-07-30 00:15:53 +0600

    Add edges to the graph

--- Main.java
+++ Main.java
@@ -9,5 +9,12 @@ public class Main {
         graph.addVertex(3);
         graph.addVertex(4);
         graph.addVertex(5);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+        graph.addEdge(4, 5);
+        System.out.println("Graph:");
+        graph.printGraph();
     }
 }
